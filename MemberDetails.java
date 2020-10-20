b3f902f185ce9ecbc15b3a53a7d62efcdeccc261
Chandrakant3 <72928559+Chandrakant3@users.noreply.github.com>
2020-10-20 15:03:23 +0530

    group members

---
+++ MemberDetails.java
@@ -0,0 +1,44 @@
+class MemberDetails
+{
+public static void main(String args [])
+{
+Details chandru = new Details();
+chandru.name = "chandru";
+chandru.place = "gokak";
+chandru.college = "bgmit";
+chandru.hobbies = "story writer";
+chandru.groupname = "teamupgrade";
+chandru.information();
+
+Details anusha = new Details();
+anusha.name = "anusha";
+anusha.place = "tarikeri";
+anusha.college ="ait";
+anusha.hobbies = "watching mythological stories";
+anusha.information();
+
+Details arpitha = new Details();
+arpitha.name = "arpitha";
+arpitha.place ="antharagatte";
+arpitha.college ="ait";
+arpitha.hobbies = "listening music";
+arpitha.groupname = "teamupgrade";
+arpitha.information();
+
+Details babitha = new Details();
+babitha.name = "babitha";
+babitha.place = "arasikeri";
+babitha.college = "ait";
+babitha.hobbies = "singing";
+babitha.groupname= "teamupgrade";
+babitha.information();
+
+Details naheeda = new Details();
+naheeda.name = "naheeda";
+naheeda.place = "jamkhandi";
+naheeda.college = "bgmit";
+naheeda.hobbies = "watching cricket";
+naheeda.groupname = "teamupgrade";
+naheeda.information();
+}
+}

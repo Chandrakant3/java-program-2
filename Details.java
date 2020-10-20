ee40d72c64785c3be66378593f51e3e96476363b
Chandrakant3 <72928559+Chandrakant3@users.noreply.github.com>
2020-10-20 15:02:01 +0530

    Add files via upload

---
+++ Details.java
@@ -0,0 +1,16 @@
+class Details 
+{
+String name;
+String place;
+String college;
+String hobbies;
+String groupname;
+void information()
+{
+System.out.println("name:"+ " "+name);
+System.out.println(" place:"+" "+place);
+System.out.println("college:"+" "+college);
+System.out.println("hobbies:"+ " "+hobbies);
+System.out.println("groupname:"+ " "+groupname);
+}
+}
